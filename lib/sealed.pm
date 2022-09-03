66f17c63741413023bed0273972251f5ba0bca94
Joe Schaefer <joe@sunstarsys.com>
2022-09-03 12:25:50 -0400

    v4.1.3:doc-fixes

--- lib/sealed.pm
+++ lib/sealed.pm
@@ -14,7 +14,7 @@ use version;
 use B::Generate ();
 use B::Deparse  ();
 
-our $VERSION                    = qv(4.1.2);
+our $VERSION                    = qv(4.1.3);
 our $DEBUG;
 
 my %valid_attrs                 = (sealed => 1);
